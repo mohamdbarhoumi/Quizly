7192ec6080adb9b4831c5e93b845b137d55c1ff5
mohamdbarhoumi <barhoumimohamed@fsg.u-gabes.tn>
2025-04-06 10:49:49 +0200

    add quiz form

--- components/quizCreation.tsx
+++ components/quizCreation.tsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { useForm } from "react-hook-form";
 import { quizCreationSchema } from "@/schemas/form/quiz";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,9 +14,10 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-
-
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { BookOpen, CopyCheck } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 type Props = {};
 type Input = z.infer<typeof quizCreationSchema>;
@@ -29,31 +31,92 @@ const QuizCreation = (props: Props) => {
       type: "open_ended",
     },
   });
+
+  function onSubmit(input: Input) {
+    alert(JSON.stringify(input, null, 2));
+  }
+
+  form.watch();
+
   return (
-    <div className="absolute top-0.5 left-0.5 -translate-x-0.5 -translate-y-0.5 ">
+    <div className="flex items-center justify-center min-h-screen">
+      {" "}
+      {/* Centering the card */}
       <Card>
         <CardHeader>
-          <CardTitle className="font-bold text-2x">Quiz Creation</CardTitle>
+          <CardTitle className="font-bold text-2xl">Quiz Creation</CardTitle>
         </CardHeader>
         <CardContent>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <FormField
                 control={form.control}
-                name="username"
+                name="topic"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Username</FormLabel>
+                    <FormLabel>Topic</FormLabel>{" "}
+                    {/* Changed from Username to Topic */}
                     <FormControl>
-                      <Input placeholder="shadcn" {...field} />
+                      <Input placeholder="Enter a topic..." {...field} />
                     </FormControl>
-                    <FormDescription>
-                      This is your public display name.
-                    </FormDescription>
+                    <FormDescription>Please provide a topic</FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
               />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Question</FormLabel>{" "}
+                    {/* Changed from Username to Topic */}
+                    <FormControl>
+                      <Input
+                        placeholder="Enter an amount..."
+                        {...field}
+                        type="number"
+                        min={1}
+                        max={10}
+                        onChange={(e) => {
+                          form.setValue("amount", parseInt(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>Please provide a topic</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between">
+                <Button
+                  type="button"
+                  className="w-1/2 rounded-none rounded-l-lg"
+                  onClick={() => {
+                    form.setValue("type", "mcq");
+                  }}
+                  variant={
+                    form.getValues("type") === "mcq" ? "default" : "secondary"
+                  }
+                >
+                  <CopyCheck className="w-4 h-4 mr-2" /> Multiple Choice
+                </Button>
+                <Separator orientation="vertical" />
+                <Button
+                  type="button"
+                  className="w-1/2 rounded-none rounded-r-lg"
+                  onClick={() => {
+                    form.setValue("type", "open_ended");
+                  }}
+                  variant={
+                    form.getValues("type") === "open_ended"
+                      ? "default"
+                      : "secondary"
+                  }
+                >
+                  <BookOpen className="w-4 h-4 mr-2" /> Oped Ended
+                </Button>
+              </div>
               <Button type="submit">Submit</Button>
             </form>
           </Form>
